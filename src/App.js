51c93ffade78b41fb9f81dafc0050d7f07c3786c
Kostya <baldakov.kostya@yandex.ru>
2023-12-11 15:16:17 +0300

    Component with createElement

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+const year = new Date().getFullYear();
 
-const year = new Date().getFullYear(); // Императивный
-
-function App() { // Императивный
-	return ( // Императивный
-		// Деклоративный подход начало
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{year}
-			</header>
-		</div>
-		// Деклоративный подход конец
-	);
+function App() {
+  return (
+    React.createElement("div", {
+      className: "App"
+    }, React.createElement("header", {
+      className: "App-header"
+    }, React.createElement("img", {
+      src: logo,
+      className: "App-logo",
+      alt: "logo"
+    }), React.createElement("p", null, "Edit ", React.createElement("code", null, "src/App.js"), " and save to reload."), React.createElement("a", {
+      className: "App-link",
+      href: "https://reactjs.org",
+      target: "_blank",
+      rel: "noopener noreferrer"
+    }, "Learn React"), year))
+  );
 }
-
 export default App;
